60f80c672fb4e5ced607d6ac902dd5704967ea9e
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-01-26 19:28:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-from aiogram.dispatcher.dispatcher import loggers
 from bot import BOT
-from aiogram.client.telegram import TelegramAPIServer
-from aiogram.client.session.aiohttp import AiohttpSession
-from logger_config import logger
 
 import asyncio
-import logger_config
+from logger_config import logger
 
 async def on_start():
     print("Bot avviato")
 
 async def on_stop():
-    
     print("Bot fermato")
 
 async def main():
@@ -20,7 +15,7 @@ async def main():
         await on_start()
         await my_bot.dp.start_polling(my_bot.bot)
     except Exception as ex:
-        logging.error(f"Errore durante l'esecuzione di handle_set_state: {ex}", exc_info=True)
+        logger.error(f"Errore durante l'esecuzione di handle_set_state: {ex}", exc_info=True)
     except KeyboardInterrupt:
         print("Interrotto dall'utente")
     finally:
