459089902018833efe4f3c7f9945e94629eabca3
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-01-26 19:31:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import logging
 
-TOKEN = "TOKEN" #token di prova
+TOKEN = "TOKEN" #token of test
 #TOKEN = 'TOKEN' #token produzione
 
 admin_id = 1026795763
@@ -9,9 +9,9 @@ api_base_url = 'http://localhost:8081'
 
 log_file_path = 'log.txt'  
 
-use_local_api = False  # Imposta su False se l'API non � in locale
+use_local_api = False  # Set to False if the API is not in local
 
-log_level = logging.INFO  # Livello di logging desiderato
+log_level = logging.INFO  # logger type: INFO, ERROR, DEBUG
 
 
 
